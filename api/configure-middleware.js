ddf9153f2110ea4aba1b245e6e5722aab3c13e75
adamwinzdesign <adamwinzdesign@gmail.com>
2020-02-24 16:43:47 -0600

    added migration

--- api/configure-middleware.js
+++ api/configure-middleware.js
@@ -2,8 +2,12 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+// webauth-1-guided uses cors???
+// const cors = require('cors');
+
 module.exports = server => {
   server.use(helmet());
   server.use(morgan('dev'));  
   server.use(express.json());
+  // server.use(cors());
 }
